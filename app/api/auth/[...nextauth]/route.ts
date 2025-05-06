a5fcb80e5abee6d2fc82814c84f790a6072f0245
Joshua Ozorji <ozorjijoshua@gmail.com>
2025-05-06 11:41:20 +0100

    auth deploy error fixed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,51 +1,5 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
-import dbConnect from "@/lib/dbConnect";
-import User from "@/models/user";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import clientPromise from "@/lib/mongoClient";
-
-export const authOptions: AuthOptions = {
-	adapter: MongoDBAdapter(clientPromise),
-	providers: [
-		CredentialsProvider({
-			name: "Credentials",
-			credentials: {
-				email: { label: "Email", type: "email" },
-				password: {
-					label: "Password",
-					type: "password",
-				},
-			},
-			async authorize(credentials) {
-				await dbConnect();
-
-				const user = await User.findOne({
-					email: credentials?.email,
-				});
-
-				if (!user) throw new Error("No user found");
-
-				const isValid = await bcrypt.compare(
-					credentials!.password,
-					user.password,
-				);
-				if (!isValid)
-					throw new Error("Invalid credentials");
-
-				return user;
-			},
-		}),
-	],
-	session: {
-		strategy: "jwt",
-	},
-	pages: {
-		signIn: "/auth/sign-in",
-	},
-	secret: process.env.NEXTAUTH_SECRET,
-};
+import NextAuth from "next-auth";
+import { authOptions } from "./config";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
