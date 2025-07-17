7536957a647fa35b4abb50ab53f4a6cbecd7a89d
Joshua Ozorji <ozorjijoshua@gmail.com>
2025-07-17 09:04:42 +0100

    login redirect

--- app/(user)/auth/login/page.tsx
+++ app/(user)/auth/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/components/ui/LoginForm";
 
 export default function LoginPage() {
 	return (
-		<div className='flex items-center justify-center min-h-screen px-4 py-8 md:py-10 bg-gray-50 sm:px-6 lg:px-8'>
+		<div className='flex items-center justify-center min-h-fit md:min-h-screen px-4 py-8 md:py-10 bg-gray-50 sm:px-6 lg:px-8'>
 			<div className='w-full max-w-md space-y-8'>
 				<div>
 					<h2 className='mt-6 text-3xl font-extrabold text-center text-gray-900 font-poppins'>
