16692fd6af437d5cbbc5910e7fa98e04369e5dea
Joshua Ozorji <ozorjijoshua@gmail.com>
2025-07-19 21:22:50 +0100

    deployment client error

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -3,12 +3,12 @@ import "dotenv/config";
 
 // const cache = new Map();
 
-if (!process.env.NEXT_SANITY_PROJECT_ID) {
+if (!process.env.NEXT_PUBLIC_SANITY_PROJECT_ID) {
 	throw new Error("NEXT_PUBLIC_SANITY_PROJECT_ID is not set");
 }
 
 export const client = createClient({
-	projectId: process.env.NEXT_SANITY_PROJECT_ID,
+	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
 	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
 	apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-03-23",
 	useCdn: false,
